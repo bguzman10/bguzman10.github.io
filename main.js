cd4e42e44f0d95ee4e23302f715bf568aed3c1b4
bguzman10 <43386336+bguzman10@users.noreply.github.com>
2019-10-24 11:50:30 -0500

    Fixed comment box error; made it static
    Also added JS function to show current date in mm/dd/yyyy format

--- main.js
+++ main.js
@@ -2,3 +2,4 @@ alert("Welcome to my website! Send me a comment over email to get in contact "
 + "with me, or check out some of my social media links I have included at the bottom of the page!");
 
 
+
